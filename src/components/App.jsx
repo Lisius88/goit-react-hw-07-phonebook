677d8c49d7c0e253fe9db96705316f9c276e4637
Makarov Yelisei <fenniz123@gmail.com>
2023-01-07 15:20:47 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,7 +24,7 @@ export const App = () => {
       <Filter />
       <Form />
       {isLoading && !error && <Loader />}
-      {!isLoading && <ContactsList />}
+      <ContactsList />
       <Toaster position="top-right" reverseOrder={false} />
     </MainContent>
   );
